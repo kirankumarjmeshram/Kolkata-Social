aa95615abc4f7994387422a9c3a255a79157fd31
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-02-21 14:50:45 +0530

    server started running

--- backend/src/configs/db.js
+++ backend/src/configs/db.js
@@ -1,1 +1,12 @@
 // mongodb://localhost:27017/ksocial1
+const mongoose = require("mongoose");
+//const dotenv = require("dotenv");
+//dotenv.config();
+
+const connect = () => {
+  //return mongoose.connect(process.env.MONGO_URI);
+  return mongoose.connect('mongodb://localhost:27017/ksocial1');
+};
+
+module.exports = connect;
+
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,29 @@
-const express = require("express");
-const app = express();
-app.use(express.json());
+
+const app = require("./index");
+const connect = require("./configs/db");
+
+
 
 app.get('/', (req, res) => {
-    res.send(`Kolkata Social`)
+    res.send(`
+    
+    `)
   })
     
 
-app.listen(1234,async(res,rec)=>{
-    //await connect();
-    console.log("Listening on port 1234")
-})
+// app.listen(1234,async(res,rec)=>{
+//     //await connect();
+//     console.log("Listening on port 1234")
+// })
+
+
+
+// //__________________________________
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, async () => {
+  console.log(`Listening on port ${port}`);
+  await connect();
+});
